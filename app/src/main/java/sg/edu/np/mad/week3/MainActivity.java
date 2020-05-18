e59e7e66edeac88a3898baa49fa5b2689bbc79a0
Qing Hua Yukie Ang <s10195775@connect.np.edu.sg>
2020-05-18 21:59:48 +0800

    week 3

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,7 +2,10 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //video file cannot play for some reason 
+        VideoView view = findViewById(R.id.videoView3);
+        view.setVideoURI(
+                Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid) );
+        view.start();
+
+        MediaPlayer media = MediaPlayer.create(this, R.raw.baby_laughing);
+        media.start();
+        //once done with the playback
+        if (!media.isPlaying()){
+            media.release();
+            media = null;
+        }
+
     }
 }
